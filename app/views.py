75803b208029828bbf3dfa6ef94e96d8673992b3
ycoodr <ycoodr@gmail.com>
2022-08-13 17:43:35 +0300

    form

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    task = Task.objects.all()
+    form = TaskForm()
+    return render(request, 'index.html', {'task': task, 'form': form})
