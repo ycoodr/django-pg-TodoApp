47da5989468ece51737265ddc49f054682acd112
ycoodr <ycoodr@gmail.com>
2022-08-13 19:02:57 +0300

    end

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('update_task/<str:key>', views.update_task, name='update_task'),
+    path('delete_task/<str:key>', views.delete_task, name='delete_task')
 ]
--- app/views.py
+++ app/views.py
@@ -13,3 +13,21 @@ def index(request):
     task = Task.objects.all()
     form = TaskForm()
     return render(request, 'index.html', {'task': task, 'form': form})
+
+def update_task(request, key):
+    task = Task.objects.get(id = key)
+    form = TaskForm(instance = task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance = task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    return render(request, 'update_task.html', {'form': form})
+
+def delete_task(request, key):
+    task = Task.objects.get(id = key)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('/')
+    return render(request, 'delete_task.html', {'task': task})
